0a76b967d20270cf32987ed7310243211b2be028
tyzou <345237699@qq.com>
2019-09-22 00:10:58 +0800

    branch king

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("----  切换分支     ");
+		System.out.println("----  切换分支     ---->king");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
